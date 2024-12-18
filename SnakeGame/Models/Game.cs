25155f2420d516abe993b707ada775afda7932f8
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2024-12-18 11:54:17 +0100

    Add default values to Game.cs

--- SnakeGame/Models/Game.cs
+++ SnakeGame/Models/Game.cs
@@ -2,13 +2,13 @@ namespace SnakeGame.Models;
 
 public class Game
 {
-    public string Id { get; set; }
-    
-    public Ruleset Ruleset { get; set; }
-    
-    public string Map { get; set; }
+    public string Id { get; set; } = "";
+
+    public Ruleset? Ruleset { get; set; }
+
+    public string Map { get; set; } = "";
     
     public int Timeout { get; set; }
-    
-    public string Source { get; set; }
+
+    public string Source { get; set; } = "";
 }
